d7c3657b53763f55e639ef6b7562d7a0ba91e4da
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 14:01:21 +0700

    update

---
+++ resources/views/register.blade.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body{
+                margin: 0;
+                background: #39518c;
+                font-family: 'Kanit', sans-serif;
+            }
+            .center{
+                width: 90%;
+                margin: auto;
+            }
+            /*.name-con{
+                display: grid;
+                grid-template-columns: auto auto;
+            }*/
+            form{
+                width: 80%;
+                /*height: 70%;*/
+                background-color: white;
+                border-radius: 10px;
+                padding: 20px;
+                margin: 20px auto;
+                max-width: 600px;
+            }
+            form > h1 {
+                text-align: center;
+            } 
+            /*.register{
+                width: 100%;
+            }*/
+            form input{
+                border: 1px solid #39518c;
+                border-radius: 100px;
+                padding: 10px;
+                width: 90%;
+                outline: none;
+                font-family: 'Kanit', sans-serif;
+            }
+            .submit{
+                background-color: #39518c;
+                color: white;
+                border-radius: 100px;
+                padding: 10px 20px;
+                margin-top: 15px;
+                border: 0;
+                font-family: 'Kanit', sans-serif;
+            }
+            .grid{
+                display: grid;
+                grid-template-columns: auto auto;
+                grid-gap: 10px;
+            }
+            .btn-con{
+                text-align: center;
+            }
+            /*@media screen and (max-width: 700px) {
+                .name-con{
+                    grid-template-columns: auto;
+                }
+            }*/
+        </style>
+    </head>
+    <body>
+        <form>
+                <h1>สมัครเข้าร่วมสถาบันปัญญาประชารัฐ</h1>
+                <div  class="center">
+                <p>ชื่อ (First Name)</p>
+                <input type="text" required/>
+                <p>นามสกุล (Last Name)</p>
+                <input type="text" required/>
+                <p>อายุ (Age)</p>
+                <input type="text" required/>
+                <p>เบอร์โทรศัพท์ (Telephone number)</p>
+                <input type="text" required/>
+                <p>อีเมล (Email)</p>
+                <input type="text" required/>
+                <p>ที่อยู่ (Address)</p>
+                <div class="grid">
+                    <input type="text" placeholder="บ้านเลขที่/หมู่/ซอย/ถนน (Number)" required/>
+                    <input type="text" placeholder="ตำบล (Sub-district)" required/>
+                    <input type="text" placeholder="อำเภอ (District)" required/>
+                    <input type="text" placeholder="จังหวัด (Province)" required/>
+                    <input type="text" placeholder="รหัสไปรษณีย์ (Postal Code)" required/>
+                </div>
+                <p>อาชีพ (Career)</p>
+                <input type="text" required/>
+                <p>การศึกษา (Education)</p>
+                  <div class="grid">
+                    <input type="text" placeholder="คณะ (Faculty)" required/>
+                    <input type="text" placeholder="สาขา (Major)" required/>
+                </div>          
+                </div>
+                <div class="btn-con">
+                    <button class="submit">Submit</button>
+                </div>
+        </form>
+    </body>
+</html>
