a204d064dec0ae3bcafec3c61b5f76537fb834a1
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 14:17:42 +0700

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-        <title>Laravel</title>
+        <title>สถาบันปัญญาประชารัฐ</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
