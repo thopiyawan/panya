d1c5eee6856812c68dde27923839938ae0775acb
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 23:49:54 +0700

    update migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('age');
+            $table->string('tel_number');
+            $table->string('email');
+            $table->string('addr');
+            $table->string('addr_sub_dist');
+            $table->string('addr_dist');
+            $table->string('addr_prov');
+            $table->string('addr_postal_code');
+            $table->string('career');
+            $table->string('edu_faculty');
+            $table->string('edu_major');
+            $table->string('edu_place');
+            $table->string('edu_level');
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
