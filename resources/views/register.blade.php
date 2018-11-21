3c5903813c449d55e13707ba8df8efffddc50154
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 16:30:41 +0700

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,9 +9,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
-        <script language="JavaScript" type="text/javascript" src="/js/jquery-1.2.6.min.js"></script>
-        <script language="JavaScript" type="text/javascript" src="/js/jquery-ui-personalized-1.5.2.packed.js"></script>
-        <script language="JavaScript" type="text/javascript" src="/js/sprinkle.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
        
         <!-- Styles -->
         <style>
