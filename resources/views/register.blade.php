4638a5f0692a50405ab3e874b94665930dee1faa
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 15:45:08 +0700

    TEST CSS DROPDOWN

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -81,6 +81,132 @@
                     grid-template-columns: auto;
                 }
             }*/
+            * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: flex-start;
+  font-family: 'Ek Mukta';
+  text-transform: uppercase;
+  font-weight: 600;
+  letter-spacing: 4px;
+  background: #1D1F20;
+}
+
+h1 {
+  margin-top: 10vh;
+  font-size: 2.5rem;
+  max-width: 500px;
+  letter-spacing: 3px;
+  text-align: center;
+  line-height: 1.5;
+  font-family: 'Open Sans';
+  text-transform: capitalize;
+  font-weight: 800;
+  color: white;
+  
+  span {
+    color: #FF908B;
+  }
+}
+
+form {
+  position: relative;
+  width: 18rem;
+  margin-top: 8vh;
+}
+
+.chosen-value,
+.value-list {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+.chosen-value {
+  font-family: 'Ek Mukta';
+  text-transform: uppercase;
+  font-weight: 600;
+  letter-spacing: 4px;
+  height: 4rem;
+  font-size: 1.1rem;
+  padding: 1rem;
+  background-color: #FAFCFD;
+  border: 3px solid transparent;
+  transition: .3s ease-in-out;
+  
+  &::-webkit-input-placeholder {
+    color: #333;
+  }
+  
+  &:hover {
+    background-color: #FF908B;
+    cursor: pointer;
+    
+    &::-webkit-input-placeholder {
+      color: #333;
+    }
+  }
+  
+  &:focus,
+  &.open {
+    box-shadow: 0px 5px 8px 0px rgba(0,0,0,0.2);
+    outline: 0;
+    background-color: #FF908B;
+    color: #000;
+    
+    &::-webkit-input-placeholder {
+      color: #000;
+    }
+  }
+}
+
+.value-list {
+  list-style: none;
+  margin-top: 4rem;
+  box-shadow: 0px 5px 8px 0px rgba(0,0,0,0.2);
+  overflow: hidden;
+  max-height: 0;
+  transition: .3s ease-in-out;
+  
+  &.open {
+   max-height: 320px;
+   overflow: auto;
+  }
+  
+  li {
+    position: relative;
+    height: 4rem;
+    background-color: #FAFCFD;
+    padding: 1rem;
+    font-size: 1.1rem;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    transition: background-color .3s;
+    opacity: 1;
+    
+    &:hover {
+      background-color: #FF908B;
+    }
+    
+    &.closed {
+      max-height: 0;
+      overflow: hidden;
+      padding: 0;
+      opacity: 0;
+    }
+  }
+}
         </style>
     </head>
     <body>
@@ -110,15 +236,16 @@
                 <input type="text" required/>
                 <p>การศึกษา (Education)</p>
                   <div class="grid">
-                    <select name="edu" id="edu">
-                        <option value=""  >- กรุณาเลือกระดับการศึกษา -</option>
-                        <option value="1" >ต่ำกว่ามัธยมศึกษา</option>
-                        <option value="2" >ปวช./มัธยมศึกษา</option>
-                        <option value="3" >ปวส.</option>
-                        <option value="4" >ปริญญาตรี</option>
-                        <option value="5" >ปริญญาโท</option>
-                        <option value="6" >ปริญญาเอก</option>
-                    </select>
+                <form>
+                  <input class="chosen-value" type="text" value="" placeholder="Type to filter">
+                  <ul class="value-list">
+                    <li>Alabama</li>
+                    <li>Alaska</li>
+                    <li>Arizona</li>
+                    <li>Arkansas</li>
+                    <li>California</li>
+                  </ul>
+                </form>
                     <input type="text" placeholder="คณะ (Faculty)" required/>
                     <input type="text" placeholder="สาขา (Major)" required/>
                     <input type="text" placeholder="โรงเรียน/มหาลัย" required/>
