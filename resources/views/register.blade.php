26bb1e7bc16450a92d670e89aa222abcb2053fcd
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 22:16:39 +0700

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -74,6 +74,8 @@
                 width: 100%;
              /*   outline: none;*/
                 font-family: 'Kanit', sans-serif;
+                min-height: 3em;
+                max-height:3em;
             }
             /*@media screen and (max-width: 700px) {
                 .name-con{
