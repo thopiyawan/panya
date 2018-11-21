1cce9e0d4df171d430032ff0f82b39eed9ad4017
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2018-11-21 16:28:12 +0700

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,6 +9,9 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+        <script language="JavaScript" type="text/javascript" src="/js/jquery-1.2.6.min.js"></script>
+        <script language="JavaScript" type="text/javascript" src="/js/jquery-ui-personalized-1.5.2.packed.js"></script>
+        <script language="JavaScript" type="text/javascript" src="/js/sprinkle.js"></script>
        
         <!-- Styles -->
         <style>
